30f843463dcabf2049157f3cd097aae083924ec6
zi-kai-lin <zikailin317@gmail.com>
2025-06-05 17:22:51 +0800

    added docker set up for mysql and phpmyadmin, with app ran based on nodemon

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY nodemon.json ./
+
+COPY . .
+
+EXPOSE 3000
+
+
+CMD ["npm", "run", "dev"]
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
+    console.log("Changes")
 
     console.log("backend server listening on port", PORT)
 
